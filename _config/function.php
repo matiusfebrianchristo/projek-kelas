1e7e37eca1a018bb4a1f7a1d5214535e1d644f78
matiusfebrianchristo <chriss.tkj@gmail.com>
2022-01-21 06:04:25 +0700

    Refisi 1

--- _config/function.php
+++ _config/function.php
@@ -1,60 +1,159 @@
 <?php
+	// ===========================================================================================
+	// Koneksi ke Database MySQL
 	
-	require 'db_config.php';
+	// Mendefinisikan host, user, password, nama_database untuk login ke Database 
+	$conn = mysqli_connect("localhost", "root", "", "data_siswa");
+
+	// cek apakah berhasil atau tidak
+	if (!$conn) {
+		die("Connection Error: " . mysqli_connect_error());
+	} 
+
+
+	// ===================================================================================== 
+	// Untuk Function 
 	
-	// For GET data from Database 
-	// For GET Query
+	// Function untuk mengambil data dari Database
 	function query($query)
 	{
+		// Untuk mengambil variable Koneksi yang berada dari luar
 		global $conn;
 
+		// untuk mengambil Data dari database
 		$result = mysqli_query($conn, $query);
+		// variabel untuk menyimpan data yang diambil dari Database 
 		$list = [];
+
+		// Untuk memasukan dengan cara perulangan
+		// Semua program akan memasukan data satu per satu ke dalam variabel list atau variabel untuk menyimpan data dari database
 		while ($data = mysqli_fetch_assoc($result)) {
 			$list[] = $data;
 		}
+
+		// Mengembalikan nilai variabel list
 		return $list;
 	}
 
 	// ========================================
-	// Add Students
+	// Function untuk Menambahkan Siswa
 	function addStudent($data) {
+
+		//Untuk mengambil Variabel Koneksi yang berada di luar function 
 		global $conn;
 
+		// Mendefinisikan varibel untuk input nama, nisn, kelas, absen
+		// htmlspecialchars digunakan untuk mencegah terjadi nya sebuah error ketika user menginput kan tag html atau baris html
+		// $data adalah argumen dimana nanti nya dapat diisi saat memanggil function tersebut
 		$name = htmlspecialchars($data["nama"]);
 		$class = htmlspecialchars($data["kelas"]);
 		$absen = htmlspecialchars($data["absen"]);
 		$nisn = htmlspecialchars($data["nisn"]);
 
+		// Mendefinisikan perintah database untuk menambahkan data ke dalam Database
+		// INSERT INTO siswa (nama_tabel) VALUES (data_yang_akan_diisikan)
 		$query = "INSERT INTO siswa (nisn, nama, kelas, nomor_absen) VALUES ('$nisn', '$name', '$class', '$absen')";
 
+		// Untuk menjalan atau mengeksekusi perintah mysql yang telah di definisikan tadi 
+		// mysqli_query(koneksi, perintah)
 		mysqli_query($conn, $query);
+
+		// Mengembalikan nilai berupa angka dimana jika berhasil maka nilai yg dikembalikan adalah 1 jika gagal -1
 		return mysqli_affected_rows($conn);
 
 	}
 
 	// ==========================================
-	// Add Class
+	// Tambah Kelas
 	function addClass($data) {
+		//Untuk mengambil Variabel Koneksi yang berada di luar function 
 		global $conn;
 
+		// Mendefinisikan varibel untuk input nama, nisn, kelas, absen
+		// htmlspecialchars digunakan untuk mencegah terjadi nya sebuah error atau kesalahan ketika user menginput kan tag html atau baris html
+		// $data adalah argumen dimana nanti nya dapat diisi saat memanggil function tersebut
 		$kelas = htmlspecialchars($data["kelas"]);
 
+		// Mendefinisikan perintah database untuk menambahkan data ke dalam Database
+		// INSERT INTO siswa (nama_tabel) VALUES (data_yang_akan_diisikan)
 		$query = "INSERT INTO kelas (id_kelas, nama_kelas) VALUES ('', '$kelas')";
 
+		// Untuk menjalan atau mengeksekusi perintah mysql yang telah di definisikan tadi 
+		// mysqli_query(koneksi, perintah)
 		mysqli_query($conn, $query);
+
+		// Mengembalikan nilai berupa angka dimana jika berhasil maka nilai yg dikembalikan adalah 1 jika gagal -1
 		return mysqli_affected_rows($conn);
 	}
 
 
 	// ===========================================
-	// Detele Student
+	// Menghapus Siswa
 	function deleteStudent($nisn) {
+		//Untuk mengambil Variabel Koneksi yang berada di luar function 
 		global $conn;
 
+		// Mendefinisikan perintah database untuk menghapus data ke dalam Database
+		// DELETE FROM nama_tabel WHERE nama_attribut = data_yang_akan_diisi
 		$query = "DELETE FROM siswa WHERE nisn = $nisn";
 
+		// Untuk menjalan atau mengeksekusi perintah mysql yang telah di definisikan tadi 
+		// mysqli_query(koneksi, perintah)
 		mysqli_query($conn, $query);
+
+		// Mengembalikan nilai berupa angka dimana jika berhasil maka nilai yg dikembalikan adalah 1 jika gagal -1
+		return mysqli_affected_rows($conn);
+	}
+
+	// ==================================================
+	// Untuk mengambil nama_kelas dengan id_kelas dari Database
+	function getNamaKelas($id_kelas) {
+		//Untuk mengambil Variabel Koneksi yang berada di luar function 
+		global $conn;
+
+		// Mendefinisikan perintah database untuk mengambil data dari Database
+		// SELECT * FROM nama_tabel WHERE nama_attribut = data_yang_akan_diisi
+		$query = "SELECT * FROM kelas WHERE id_kelas = '$id_kelas'";
+
+		// Mendefinisikan variabel untuk menyimpan hasil pengambilan data dari Database 
+		// mysqli_query(variabel_koneksi, perintah)		
+		$data = mysqli_query($conn, $query);
+
+		// Untuk menjalan atau mengeksekusi perintah mysql yang telah di definisikan tadi
+		// $data = mysqli_fetch_assoc($nama_kelas);
+		$kelas = mysqli_fetch_assoc($data);
+
+		// Mengembalikan nilai dari variabel $kelas yang berisi data dari Database
+		return $kelas['nama_kelas'];
+
+	}
+
+	//=======================================================
+	// Untuk update atau edit data Siswa
+	function updateSiswa($data) {
+		// Untuk mengambil Variabel Koneksi yang berada di luar function 
+		global $conn;
+
+		// Mendefinisikan variable untuk data yg akan dimasukan
+		$nisn = $data['nisn'];
+		$nama = $data['nama'];
+		$kelas = $data['kelas'];
+		$nomor_absen = $data['absen'];
+
+		// Mendefinisikan perintah database untuk update data dari Database
+		// UPDATE nama_tabel SET nama_attribut = data_yang_akan_diisikan WHERE nama_attribut = data_yang_akan_diisikan
+		$query = "UPDATE siswa SET nama = '$nama', kelas = '$kelas', nomor_absen = '$nomor_absen' WHERE nisn = '$nisn'";
+
+		// Untuk menjalan atau mengeksekusi perintah mysql yang telah di definisikan tadi
+		// mysqli_query(variabel_koneksi, perintah)
+		$data = mysqli_query($conn, $query);
+		// var_dump($data);
+
+		if (!$data) {
+			return "Error : " . mysqli_error($data);
+		}
+
+		// Mengembalikan nilai dari variabel $kelas yang berisi data dari Database
 		return mysqli_affected_rows($conn);
 	}
 
--- index.php
+++ index.php
@@ -1,101 +1,106 @@
 <?php
- include '_config/function.php';
+include '_config/function.php';
 
- $list_student = query(constant("QUERY_SISWA"));
+ // Mengambil data dari tabel Siswa
+$list_student = query("SELECT * FROM siswa");
 
- $list_kelas = query(constant("QUERY_KELAS"));
- 
- $number = 1;
+ // Mengambil data dari tabel kelas
+$list_kelas = query("SELECT * FROM kelas");
+
+ // Untuk numbering
+$number = 1;
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- main CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<!-- main CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap/css/bootstrap.min.css">
 
-    <!-- Fontawesome -->
-    <script defer src="assets/fontawesome/js/all.js"></script>
+	<!-- Fontawesome -->
+	<script defer src="assets/fontawesome/js/all.js"></script>
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-  	<!-- Main Page -->
-   	<div class="container mt-3">   	
-   		<h1 class="text-center">Data Siswa</h1>
+	<title>Hello, world!</title>
+</head>
+<body>
+	<!-- Main Page -->
+	<div class="container mt-3">   	
+		<h1 class="text-center">Data Siswa</h1>
 
-   		<!-- =========================================================================== -->
-   		<!-- Modal For Add Student -->
-   		<!-- Button trigger modal -->
-   		<div class="d-flex w-100 m-2">
+		<!-- =========================================================================== -->
+		<!-- Modal For Add Student -->
+		<div class="d-flex w-100 m-2">
 			<a href="tambah_kelas.php" class="btn btn-secondary rounded-12 me-auto">
-			  Tambah Kelas <i class="fas fa-plus-circle"></i>
+				Tambah Kelas <i class="fas fa-plus-circle"></i>
 			</a>
 			<a href="tambah_siswa.php" class="btn btn-primary rounded-12 ms-auto" style="margin-right: 1.2rem;">
-			  Tambah Siswa <i class="fas fa-plus-circle"></i>
+				Tambah Siswa <i class="fas fa-plus-circle"></i>
 			</a>
-   		</div>
+		</div>
 
 
 		<!-- ======================================================================= -->
-   		<!-- List Students -->
-   		<?php if(count($list_kelas) == 0 ) : ?>
-   			<p class="text-danger text-center">Kelas belum ada. Tambahkan kelas terlebih dahulu.</p>
-   		<?php endif; ?>
-	  	<div class="table-responsive mt-3">
-		  <table class="table table-bordered table-striped text-center">
-		    <thead>
-		    	<tr>
-		    		<th>No.</th>
-		    		<th class="mw-10 coloums">Nama <i class="fas fa-sort"></i>
-		    		<th class="mw-absen coloums">No. Absen <i class="fas fa-sort"></i>
-		    		<th class="mw-class">Kelas</th>
-		    		<th>NISN</th>
-		    		<th class="mw-8">Aksi</th>
-		    	</tr>
-		    </thead>
-		    <tbody>
-		    	<?php if (count($list_student) !== 0) {
-		    		foreach ( $list_student as $data ) :   ?>
-		    	<tr>
-		    		<td><?= $number++; ?></td>
-		    		<td class="text-start pl-1 "><?= $data['nama']; ?></td>
-		    		<td><?= $data['nomor_absen']; ?></td>
-		    		<td><?= $data['nama_kelas']; ?></td>
-		    		<td><?= $data['nisn']; ?></td>
-		    		<td>
-		    			<a href="edit.php?nisn=<?= $data['nisn'] ?>" class="mx-md-3 "><i class="fas fa-edit icon edit"></i></a>
-		    			<a href="hapus.php?nisn=<?= $data['nisn'] ?>" class="mx-md-3 "><i class="fas fa-trash icon delete"></i></a>
-		    		</td>
-		    	</tr>
-		    	<?php endforeach ?>
-		    	<?php } else { ?>
-		    		<tr>
-		    			<td colspan="6" class="text-center">Data belum ada</td>
-		    		</tr>
-		    	<?php } ?>
-		    </tbody>
-		  </table>
-		</div>
+		<!-- List Students -->
+		<!-- Cek apakah ada siswa atau tidak -->
+		<?php if(count($list_kelas) == 0 ) : ?>
+			<p class="text-danger text-center">Kelas belum ada. Tambahkan kelas terlebih dahulu.</p>
+		<?php endif; ?>
+		<div class="table-responsive mt-3">
+			<table class="table table-bordered table-striped text-center">
+				<thead>
+					<tr>
+						<th>No.</th>
+						<th class="mw-10 coloums">Nama <i class="fas fa-sort"></i>
+							<th class="mw-absen coloums">No. Absen <i class="fas fa-sort"></i>
+								<th class="mw-class">Kelas</th>
+								<th>NISN</th>
+								<th class="mw-8">Aksi</th>
+							</tr>
+						</thead>
+						<tbody>
+							<!-- JIka ada siswa maka masukan data dari database kedalam tabel -->
+							<?php if (count($list_student) !== 0) {
+								foreach ( $list_student as $data ) :   ?>
+									<tr>
+										<td><?= $number++; ?></td>
+										<td class="text-start pl-1 "><?= $data['nama']; ?></td>
+										<td><?= $data['nomor_absen']; ?></td>
+										<!-- Mengambil nama kelas dengan id_kelas yang telah ada pada tabel siswa -->
+										<td><?= getNamaKelas($data['kelas']); ?></td>
+										<td><?= $data['nisn']; ?></td>
+										<td>
+											<a href="edit.php?nisn=<?= $data['nisn'] ?>" class="mx-md-3 "><i class="fas fa-edit icon edit"></i></a>
+											<a href="hapus.php?nisn=<?= $data['nisn'] ?>" class="mx-md-3 "><i class="fas fa-trash icon delete"></i></a>
+										</td>
+									</tr>
+								<?php endforeach ?>
+							<?php } else { ?>
+								<tr>
+									<td colspan="6" class="text-center">Data belum ada</td>
+								</tr>
+							<?php } ?>
+						</tbody>
+					</table>
+				</div>
 
-   	</div>		
-
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-    <script src="https://kit.fontawesome.com/42150c805c.js" crossorigin="anonymous"></script>
-    <script  src="assets/js/main.js"></script>
+			</div>		
 
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
+
+			<!-- Optional JavaScript; choose one of the two! -->
+			<script src="https://kit.fontawesome.com/42150c805c.js" crossorigin="anonymous"></script>
+			<script  src="assets/js/main.js"></script>
+
+
+			<!-- Option 1: Bootstrap Bundle with Popper -->
+			<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+		</body>
+		</html>
